55b209332fcb3430b12586dc4823e14d9991b2d5
colinbrust <yEoNoE5Ydrsd8c>
2019-11-07 18:38:56 -0700

    Added functions to tune hyperparameters for all cases.

--- analysis/ee_data_extract.py
+++ analysis/ee_data_extract.py
@@ -9,10 +9,13 @@ ee.Initialize()
 
 def extract_random_pts():
 
+     # import CONUS roi
     roi = uf.get_roi()
 
+    # create imageCollection of Gridmet meteorology and SMAP soil moisture data
     stack = uf.get_data()
 
+    # Create list of dates to iterate over
     date_start = dt.datetime.strptime('2016-01-01', '%Y-%m-%d')
     date_end = dt.datetime.strptime('2018-12-31', '%Y-%m-%d')
     diff = date_end - date_start
@@ -24,32 +27,36 @@ def extract_random_pts():
 
     for d in dates:
 
+        # EarthEngine tends to throw random memory errors.
+        # Use this loop to prevent a memory error from stopping analysis.
         while True:
             try:
+                # EarthEngine uses a default seed for the randomPoints function, so generate a new random number
+                # at every iteration so that every day has a different sample of points.
                 rand_seed = np.random.randint(low=0, high=20000)
+                # Create 500 random points across CONUS
                 pts = ee.FeatureCollection.randomPoints(
                     region=roi,
                     points=500,
                     seed=rand_seed
                 )
 
+                # Filter image collection based on the date
                 img = stack.filterDate(str(d.date())).first()
 
+                # Sample the pixels underneath random points
                 plot_sample_regions = img.sampleRegions(
                     collection=pts,
                     scale=9000
                 )
 
+                # Extract the data to a Pandas DataFrame
                 data = plot_sample_regions.getInfo()
 
                 df = pd.DataFrame([x['properties'] for x in data['features']])
                 df_out = pd.concat([df_out, df], axis=0, ignore_index=True)
 
-                if i % 50 == 0:
-                    df_out.to_csv('/mnt/e/Data/sm_ml_data/temp_dat_{}.csv'.format(str(i)), index=False)
-
                 print(d, i)
-                i += 1
             except ee.ee_exception.EEException as e:
                 print(e)
                 continue
--- analysis/sm_rf_pred.py
+++ analysis/sm_rf_pred.py
@@ -1,15 +1,17 @@
 import pandas as pd
 import numpy as np
 import pydot
+import pickle
 from pprint import pprint
 from sklearn.model_selection import train_test_split, RandomizedSearchCV
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_squared_error,  r2_score
 from sklearn.tree import export_graphviz
 
 
-# Created with help from https://towardsdatascience.com/random-forest-in-python-24d0893d51c0
-def run_rf_model(df, cols=None, rz=True):
+# Read in data, convert to np arrays, and then split into train and test sets
+def gather_split_data(df, cols=None, rz=True):
+
     df = pd.read_csv(df)
     df = df.sample(frac=1)
     df = df.head(10000)
@@ -21,26 +23,32 @@ def run_rf_model(df, cols=None, rz=True):
 
     if cols is not None:
         df = df[cols]
-
-    df = df.drop(['rzMean', 'surfMean'], axis=1)
+    else:
+        df = df.drop(['rzMean', 'surfMean'], axis=1)
     feat_names = list(df.columns)
 
     x_train, x_test, y_train, y_test = train_test_split(df.values, labels, test_size=0.25, random_state=1)
-    rf = RandomForestRegressor(n_estimators=1000, random_state=1)
+
+    return [x_train, x_test, y_train, y_test, feat_names]
+
+
+# run baseline model
+def run_rf_model(train_test, n_estimators=2000, min_samples_split=10, min_samples_leaf=5, max_features='auto',
+                 max_depth=None, bootstrap=True):
+
+    x_train, x_test, y_train, y_test, feat_names = train_test
+
+    rf = RandomForestRegressor(n_estimators=n_estimators, min_samples_split=min_samples_split,
+                               min_samples_leaf=min_samples_leaf, max_features=max_features,
+                               max_depth=max_depth, bootstrap=bootstrap)
     rf.fit(x_train, y_train)
 
-    out_dict = {'feat_names': feat_names,
-                'x_train': x_train,
-                'x_test': x_test,
-                'y_train': y_train,
-                'y_test': y_test,
-                'model': rf}
-
-    return out_dict
+    return rf
 
 
+# Visualize the tree created.
 def visualize_tree(rf_model, feat_names):
-
+    # Created with help from https://towardsdatascience.com/random-forest-in-python-24d0893d51c0
     tree = rf_model.estimators_[5]
 
     export_graphviz(tree, out_file='tree.dot', feature_names=feat_names, rounded=True, precision=1)
@@ -48,49 +56,29 @@ def visualize_tree(rf_model, feat_names):
     graph.write_png('tree.png')
 
 
+# sort variables based on importance.
 def get_variable_importance(rf_model, feat_names):
-
+    # Created with help from https://towardsdatascience.com/random-forest-in-python-24d0893d51c0
     imp = list(rf_model.feature_importances_)
     feature_imp = [(feature, round(importance, 2)) for feature, importance in zip(feat_names, imp)]
     feature_imp = sorted(feature_imp, key=lambda x: x[1], reverse=True)
     [print('Variable: {:20} Importance: {}'.format(*pair)) for pair in feature_imp];
 
 
-def tune_hyperparameters(df, rz=True, cols=None):
+# Create a random grid of hyperparameters to figure out what works the best
+def tune_hyperparameters(train_test):
 
-    df = pd.read_csv(df)
-    df = df.sample(frac=1)
-    df = df.head(10000)
+    x_train, x_test, y_train, y_test, feat_names = train_test
 
-    if rz:
-        labels = df['rzMean'].values
-    else:
-        labels = df['surfMean'].values
+    max_d = [int(x) for x in np.linspace(10, 110, num=11)]
+    max_d.append(None)
 
-    if cols is not None:
-        df = df[cols]
-
-    df = df.drop(['rzMean', 'surfMean'], axis=1)
-    feat_names = list(df.columns)
-
-    x_train, x_test, y_train, y_test = train_test_split(df.values, labels, test_size=0.25, random_state=1)
-
-    # n_estimators = [int(x) for x in np.linspace(start=200, stop=2000, num=10)]
-    n_estimators = [500, 1000, 2000]
-    max_features = ['auto', 'sqrt']
-    # max_depth = [int(x) for x in np.linspace(10, 110, num=11)]
-    # max_depth.append(None)
-    max_depth = [3, None]
-    min_samples_split = [2, 5, 10]
-    min_samples_leaf = [1, 5, 10]
-    bootstrap = [True, False]
-
-    random_grid = {'n_estimators': n_estimators,
-                   'max_features': max_features,
-                   'max_depth': max_depth,
-                   'min_samples_split': min_samples_split,
-                   'min_samples_leaf': min_samples_leaf,
-                   'bootstrap': bootstrap}
+    random_grid = {'n_estimators': [int(x) for x in np.linspace(start=200, stop=2000, num=10)],
+                   'max_features': ['auto', 'sqrt'],
+                   'max_depth': max_d,
+                   'min_samples_split': [2, 5, 10],
+                   'min_samples_leaf': [1, 5, 10],
+                   'bootstrap': [True, False]}
 
     rf = RandomForestRegressor()
     rf_grid = RandomizedSearchCV(estimator=rf, param_distributions=random_grid,
@@ -98,16 +86,60 @@ def tune_hyperparameters(df, rz=True, cols=None):
 
     rf_grid.fit(x_train, y_train)
 
+    return rf_grid
 
 
+# Look at model accuracy
+def evaluate(model, test_features, test_labels):
+    # Created with help from https://towardsdatascience.com/random-forest-in-python-24d0893d51c0
+    predictions = model.predict(test_features)
+    errors = abs(predictions - test_labels)
+    mape = 100 * np.mean(errors / test_labels)
+    rmse = np.sqrt(mean_squared_error(test_labels, predictions))
+    accuracy = 100 - mape
+    r2 = r2_score(test_labels, predictions)
+    print('Model Performance')
+    print('Average Error: {:0.4f} degrees.'.format(np.mean(errors)))
+    print('Accuracy = {:0.2f}%.'.format(accuracy))
+
+    return {'mae': errors,
+            'accuracy': accuracy,
+            'rmse': rmse,
+            'r2': r2}
 
 
+# Create a model for range of scenarios, tune hyperparameters, run the best model.
+def train_model(df, method='rz'):
 
-df = '/mnt/e/Data/sm_ml_data/smap_gm_data.csv'
-model = run_rf_model(df)
+    if method == 'rz':
+        train_test = gather_split_data(df, cols=['tmmn', 'tmmx', 'vpd'], rz=True)
+    elif method == 'surf':
+        train_test = gather_split_data(df, cols=['rmax', 'rmin', 'vpd'], rz=False)
+    elif method == 'rz_full':
+        train_test = gather_split_data(df, cols=None, rz=True)
+    elif method == 'surf_full':
+        train_test = gather_split_data(df, cols=None, rz=False)
+    else:
+        raise ValueError("'method' argument must either be 'rz', 'surf', 'rz_full', or 'surf_full'.")
 
-get_variable_importance(model['model'], model['feat_names'])
-# pred = rf.predict(x_test)
-# rmse = np.sqrt(mean_squared_error(y_test, pred))
-# mae = abs(pred - y_test)
-# acc = 100 - np.mean(100 * (mae / y_test))
+    model_tuned = tune_hyperparameters(train_test)
+    params = model_tuned.best_params_
+
+    opt_model = run_rf_model(train_test, **params)
+
+    out_dict = {'train_test': train_test,
+                'opt_model': opt_model,
+                'opt_params': params}
+
+    pickle.dump(out_dict, open('/home/colin.brust/workspace/data/sm_ml_data/'+method+'_trained.pickle', 'wb'))
+
+    return out_dict
+
+
+# df = '/mnt/e/Data/sm_ml_data/smap_gm_data.csv'
+dat = '/home/colin.brust/workspace/data/sm_ml_data/smap_gm_data.csv'
+
+rz_model = train_model(dat, 'rz')
+surf_model = train_model(dat, 'surf')
+rz_full_model = train_model(dat, 'rz_full')
+surf_full_model = train_model(dat, 'surf_full')
